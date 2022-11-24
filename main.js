34451679e017493bb29f26f811c67d3aecadd3e3
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2022-11-24 23:03:39 +0100

    product page

--- main.js
+++ main.js
@@ -23,3 +23,23 @@ buttonMenu.addEventListener("click", () => {
    formSearch.classList.toggle("hidden")
 })
 
+// dropdown toggle
+
+var menu2 = document.getElementById("menu");
+
+// open/close the menu when the user clicks on the button
+function toggleMenu() {
+   if (menu2.classList.contains('hidden')) {
+      menu2.classList.remove('hidden');
+   } else {
+      menu2.classList.add('hidden');
+   }
+}
+
+// close the menu when the user clicks outside of it
+window.onclick = function (event) {
+   var dropdownWrapper = document.getElementById('dropdown-wrapper');
+   if (!dropdownWrapper.contains(event.target) && !menu2.classList.contains('hidden')) {
+      menu2.classList.add('hidden');
+   }
+}
