ee093e77122ff44e3e991883feb2d7219036ec05
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2022-12-01 22:40:22 +0100

    creates single post

--- js/settings/api.js
+++ js/settings/api.js
@@ -7,7 +7,7 @@ const API_BASE_URL = "https://api.noroff.dev";
 const CREATE_USER_URL = API_BASE_URL + "/api/v1/auction/auth/register";
 const LOGIN_USER_URL = API_BASE_URL +  "/api/v1/auction/auth/login";
 const CREATE_LISTING_URL = API_BASE_URL +  "/api/v1/auction/listings";
-const GET_LISTINGS_BY_ID_URL = API_BASE_URL +  "/api/v1/auction/listings/${listingId}/bids";
+const GET_LISTINGS_BY_ID_URL = API_BASE_URL +  `/api/v1/auction/listings/bids`;
 const GET_ALL_POSTS_URL = API_BASE_URL +  "/api/v1/auction/listings";
 
 
--- js/single-listing.js
+++ js/single-listing.js
@@ -4,10 +4,28 @@ import {getToken} from "./utils/storage";
 const paramString = window.location.search;
 const searchParam = new URLSearchParams(paramString);
 const listingId = searchParam.get("listing_id");
-const listingDetails = document.querySelector('#listing-container');
-const accessToken = getToken();
-
 console.log("listingId: ", listingId);
+const accessToken = getToken();
+const listingDetails = document.querySelector('#listing-container');
+
+
+const listTitle = document.querySelector("#list-title");
+const listId = document.querySelector("#list-id");
+const listDesc = document.querySelector("#list-desc");
+const listBids = document.querySelector("#list-bids");
+const listMedia = document.querySelector("#list-media");
+const listEndsAt = document.querySelector("#list-endsAt");
+const listTags = document.querySelector("#list-tags");
+console.log(listTitle)
+console.log(listId)
+console.log(listDesc)
+console.log(listBids)
+console.log(listMedia)
+console.log(listEndsAt)
+console.log(listTags)
+
+
+
 
 
 async function getListById() {
@@ -19,36 +37,36 @@ async function getListById() {
         }
     })
     if (!accessToken) {
-        location.href = '/products.html';
+     //   location.href = '/products.html';
     }
+    console.log(response);
     const data = await response.json();
-    const postTitle = data.title;
-    const postDescription = data.description;
-    const postMedia = data.media[0];
-    const postTags = data.tags;
-    const PostCreated = data.created;
-    const postEndsAt = data.endsAt;
-    const postBids = data._count.bids;
+    const listTitle = data.title;
+    const listId = data.id;
+    const listDesc = data.description;
+    const listBids = data._count.bids;
+    const listMedia = data.media[0];
+    const listEndsAt = data.endsAt;
+    const listTags = data.tags;
 
 
     listingDetails.innerHTML = `
   
 <div class="group relative">
-                    <a href="/listing-details.html?listings_id=${post.id}">
-                        <img src="img/audi.jpg" alt="an image of white audi." class="h-full w-full object-cover object-center lg:h-full lg:w-full" src="${postMedia}">
+                    <a href="/single-listing.html?listings_id=${data.id}">
+                        <img src="img/audi.jpg" alt="an image of white audi." class="h-full w-full object-cover object-center lg:h-full lg:w-full" src="${listMedia}">
                          </a>
                     <div class="mt-4 flex justify-between">
                         <div>
                             <h3 class="text-sm font-medium text-black">
-                                <a href="#">${postTitle}</a>
+                                <a href="#">${listTitle}</a>
                             </h3>
-                             <p class="text-sm  text-gray-500">Title:${postDescription}</p>
-                             <p class="text-sm  text-gray-500">Tags:${postTags}</p>
-                            <p class="text-sm  text-gray-500"> ${postEndsAt}</p>
-                            <p class="text-sm  text-gray-500"> Current Bids: ${postBids} </p>
-                            <p class="text-sm text-red-500"><ion-icon name="time-outline"></ion-icon> Created: ${PostCreated}</p>
+                             <p class="text-sm  text-gray-500">Title:${listDesc}</p>
+                             <p class="text-sm  text-gray-500">Tags:${listTags}</p>
+                            <p class="text-sm  text-gray-500"> ${listEndsAt}</p>
+                            <p class="text-sm  text-gray-500">${listBids} </p>
                         </div>
-                        <a href="listing-details.html"><button type="button" class="inline-flex items-center rounded border border-transparent bg-cyan-500 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">Bid</button></a>
+                        <a href="/single-listing.html"><button type="button" class="inline-flex items-center rounded border border-transparent bg-cyan-500 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">Bid</button></a>
                     </div>
                 </div>
     `;
